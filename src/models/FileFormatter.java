7ba604768cd1b24be08e24b7747e46fa71b2fd23
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-11-01 20:23:47 -0300

    Minor adjustments in classes formatting

--- src/models/FileFormatter.java
+++ src/models/FileFormatter.java
@@ -1,7 +1,6 @@
 package models;
 
 public class FileFormatter {
-
   public String serializeText(String text) {
     String serializedText;
 
---
+++ src/views/MainView.java
@@ -0,0 +1,10 @@
+package views;
+
+import controllers.Controller;
+import java.util.Scanner;
+
+public class MainView {
+  public void print() {
+    Controller controller = new Controller();
+  }
+}
