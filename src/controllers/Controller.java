b362e3909f0e9c538a7bd7cf1ce0ea1a1147143e
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-11-01 19:07:15 -0300

    Create initial MVC pattern

---
+++ src/controllers/Controller.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import models.FileReader;
+
+public class Controller {
+
+  public void printHello() {
+    FileReader fileReader = new FileReader();
+
+    fileReader.printHello();
+  }
+}
