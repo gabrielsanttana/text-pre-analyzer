a17d6a072378b4d7d0fa296683f322d1a936633e
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-11-02 22:19:02 -0300

    Create DigraphGenerator class

---
+++ src/models/DigraphGenerator.java
@@ -0,0 +1,14 @@
+package models;
+
+public class DigraphGenerator {
+
+  public MultiMap<String, String> generate(String[] words) {
+    MultiMap<String, String> digraph = new MultiMap<>();
+
+    for (int i = 0; i < words.length - 1; i++) {
+      digraph.add(words[i], words[i + 1]);
+    }
+
+    return digraph;
+  }
+}
