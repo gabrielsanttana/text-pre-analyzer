caac8da4b119dc132bcae50eeb98e635228c7bb3
Gabriel Santana <gabriel.gsantana7@gmail.com>
2020-11-02 23:25:35 -0300

    Update DigraphGenerator.java

--- src/models/DigraphGenerator.java
+++ src/models/DigraphGenerator.java
@@ -3,6 +3,7 @@ package models;
 public class DigraphGenerator {
 
   public MultiMap<String, String> generate(String[] words) {
+    
     MultiMap<String, String> digraph = new MultiMap<>();
 
     for (int i = 0; i < words.length - 1; i++) {
