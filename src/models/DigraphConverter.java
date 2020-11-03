53180a3c104144a2b2db4c296377f8d4c214a8fa
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-11-03 20:12:18 -0300

    add sort keys

--- src/models/DigraphConverter.java
+++ src/models/DigraphConverter.java
@@ -1,11 +1,20 @@
 package models;
 
+import java.util.*;
+
 public class DigraphConverter {
+
+  public List<String> sortKeys(MultiMap<String, String> lines) {
+    List<String> sortedKeys = new ArrayList<>(lines.getKeySet());
+    Collections.sort(sortedKeys);
+
+    return sortedKeys;
+  }
 
   public String convert(MultiMap<String, String> lines) {
     StringBuilder serializedText = new StringBuilder();
 
-    for (String nextWord : lines.getKeySet()) {
+    for (String nextWord : this.sortKeys(lines)) {
       serializedText.append(nextWord + ", " + lines.get(nextWord) + ";\n");
     }
 
