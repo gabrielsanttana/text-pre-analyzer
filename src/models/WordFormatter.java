99fbf540a2844efd33800739521e6a9241b4e811
vitor.marques <vitor.marques@gatec.com.br>
2020-11-06 20:00:18 -0300

    Changed FileFormatter to WordFormatter, fixed return type to list of words

--- src/models/WordFormatter.java
+++ src/models/WordFormatter.java
@@ -2,9 +2,9 @@ package models;
 
 import java.text.Normalizer;
 
-public class FileFormatter {
+public class WordFormatter {
 
-  public String serializeText(String text) {
+  public String[] serializeText(String text) {
     String serializedText;
 
     serializedText = text
@@ -15,16 +15,10 @@ public class FileFormatter {
       .replaceAll(" +", " ")
       .toLowerCase()
       .replaceAll("\r", "")
-      .replaceAll("[^a-záàâã\\-\\d ]", "a")
-      .replaceAll("[^a-zéèê\\-\\d ]", "e")
-      .replaceAll("[^a-zí\\-\\d ]", "i")
-      .replaceAll("[^a-zóôõ\\-\\d ]", "o")
-      .replaceAll("[^a-zú\\-\\d ]", "u")
-      .replaceAll("[^a-zç\\-\\d ]", "c")
       .trim();
 
     serializedText = Normalizer.normalize(serializedText, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
 
-    return serializedText;
+    return serializedText.split(" ");
   }
 }
